e56f309b0f743ff5fb6b76f53059d86c230434ff
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2023-01-15 13:53:30 +0200

    python test

--- scala-libraries-python/src/main/scala/com/baeldung/scalapy/ScalaPyDemo.scala
+++ scala-libraries-python/src/main/scala/com/baeldung/scalapy/ScalaPyDemo.scala
@@ -3,8 +3,10 @@ package com.baeldung.scalapy
 import me.shadaj.scalapy.py
 import me.shadaj.scalapy.py.SeqConverters
 
-class ScalaPyDemo {
-  val listLengthPython = py.Dynamic.global.len(List(1, 2, 3).toPythonProxy)
-  println(listLengthPython)
+object ScalaPyDemo {
+  def main(args: Array[String]) = {
+    val listLengthPython = py.Dynamic.global.len(List(1, 2, 3).toPythonProxy)
+    println(listLengthPython)
   // listLengthPython: py.Dynamic = 3
+  }
 }
